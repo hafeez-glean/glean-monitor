a96841ca45ce68eddc6baa0fd6f605e15d7033b9
hafeez-glean <hafeez@glean.com>
2025-09-05 20:51:19 +0530

    Update downtime.spec.ts

--- downtime.spec.ts
+++ downtime.spec.ts
@@ -2,12 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('Homepage is up', async ({ page }) => {
   await page.goto('https://www.glean.com', { waitUntil: 'domcontentloaded' });
-  await expect(page).toHaveTitle(/Glean/);
+  await expect(page).toHaveTitle('Work AI for all.');
 });
 
 test('Get a Demo page is up', async ({ page }) => {
   await page.goto('https://www.glean.com/get-a-demo', { waitUntil: 'domcontentloaded' });
-  await expect(page.locator('h1')).toContainText('Get a Demo');
+  await expect(page.locator('h1')).toContainText('Put AI to work. At work.');
 });
 
 test('Blog page is up', async ({ page }) => {
